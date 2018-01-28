2a4df5ef117b9052df50fce06ff31f57e20d7950
Brian Martin <phir3fx@gmail.com>
2018-01-28 13:29:55 -0700

    Added skeleton adder code closes #35

--- sprint1/App.js
+++ sprint1/App.js
@@ -1,21 +1,71 @@
-class Header extends React.Component{
-  render() {
-    return (
-      <div>
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    /* state variables */
+    this.state = { 
+      sum: "",
+      operand1: "", 
+      operand2: ""
+    };
+    /* must bind all functions in constructor */
+    this.calc = this.calc.bind(this);
+    this.updateOperand1 = this.updateOperand1.bind(this);
+    this.updateOperand2 = this.updateOperand2.bind(this);
+  }
 
-      </div>
+  updateOperand1(event) {
+    /* update the value of operand 1.  needs validation */
+    this.setState({operand1 : event.target.value});
+    this.setState({sum : Number(event.target.value) + Number(this.state.operand2) })
+  }
+
+  updateOperand2(event) {
+    /* update the value of operand 2.  needs validation */
+    this.setState({operand2 : event.target.value});
+    this.setState({sum : Number(this.state.operand1) + Number(event.target.value) })
+ }
+
+  calc(event) {
+    /* Operands are text.  Must convert to add rather than concatenate. */
+    this.setState({sum : Number(this.state.operand1) + Number(this.state.operand2) })
+    event.preventDefault();
+  }
+
+  render() {
+    /* a simple form with text input and a submit button  */
+    return (
+      <form className="form-inline" onSubmit={this.calc}>
+
+        <input type="text" className="text-right form-control mr-sm-2" 
+          value={this.state.operand1} onChange={this.updateOperand1}/> 
+
+        <button className="btn btn-secondary mr-sm-2" disabled>+</button>
+
+        <input type="text" className="text-right form-control mr-sm-2" 
+          value={this.state.operand2} onChange={this.updateOperand2}/> 
+
+        <button className="btn btn-primary mr-sm-2" type="submit" value="submit" 
+          disabled>=</button>
+
+        <input type="text" className="text-right form-control mr-sm-2" 
+          value={this.state.sum} disabled/>
+      </form>
+
+    )
+  } 
+}
+
+class Application extends React.Component {
+  render() {
+    /* separate the page layout from the calculator function */
+    return (
+      <div className="jumbotron">
+        <h3>CS 314 - Simple Adder</h3>
+        <hr/>
+        <Calculator />
+      </div>  
     )
   }
 }
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("application"));
