72295cc1fb58f30d9db3b6e2563ba6030ed37898
Jason Stock <stock@localhost.localdomain>
2018-02-14 01:33:43 -0700

    Read in Colorado.svg toString

--- server/src/main/java/com/tripco/t10/planner/Trip.java
+++ server/src/main/java/com/tripco/t10/planner/Trip.java
@@ -5,7 +5,10 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.tripco.t10.server.HTTP;
 import spark.Request;
+
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The Trip class supports TFFI so it can easily be converted to/from Json by Gson.
@@ -31,12 +34,20 @@ public class Trip {
     System.out.println("TRIP.java - trip.title: " + this.title + ", type: " + this.type);
   }
 
+  private String readInSVG() {
+    InputStream istr = getClass().getResourceAsStream("/Colorado.svg");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(istr));
+    Scanner s = new Scanner(istr).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+  }
+
   /**
    * Returns an SVG containing the background and the legs of the trip.
    * @return
    */
   private String svg() {
-
+    String ColoradoBG = readInSVG();
+    //* System.out.println(ColoradoBG); Verification of Colorado.svg */
     // hardcoded example
     return "<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>";
   }
@@ -51,7 +62,7 @@ public class Trip {
     ArrayList<Integer> dist = new ArrayList<Integer>();
     if (this.places != null) {
       if (!this.places.get(0).latitude.isEmpty()) { /* Temp. while still integrating itinerary. */
-        CoordinateDistance distance = new CoordinateDistance();
+        CoordinateDistance distance = new CoordinateDistance(this.options.distance);
 
         for (int i = 0; i < this.places.size() - 1; i++) /* Append all dest1 < - > dest2 to dist */
           dist.add(distance.greatCirDist(this.places.get(i).latitude, this.places.get(i).longitude,
