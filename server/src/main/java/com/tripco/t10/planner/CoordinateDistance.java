b15818753e87b8f60445e9492f0cfc659403bb2c
Evan Steiner <evanjs@localhost.localdomain>
2018-02-17 14:56:12 -0700

    Reorganizing code and test cases to make more modular

--- server/src/main/java/com/tripco/t10/planner/CoordinateDistance.java
+++ server/src/main/java/com/tripco/t10/planner/CoordinateDistance.java
@@ -14,109 +14,7 @@ public class CoordinateDistance {
   /*Converts passed location string array to decimal degrees
   Returns -1000 if conversion fails
    */
-  public double convertDecimalDegrees(double[] location){
-    double decimalDegrees = 0;
-    double divisor = 1.0;
-    for(double d : location){
-      decimalDegrees += (d/divisor);
-      divisor *= 60;
-    }
-    return decimalDegrees;
-  }
 
-  //returns direction present in string or 0 if it doesn't have one.
-  //coordinate should be inputted so that character is at end of string
-  public char hasDirection(String coordinate){
-    if(coordinate.length() > 0) {
-      char c = coordinate.charAt(coordinate.length() - 1);
-      if (c == 'N' || c == 'S' || c == 'W' || c == 'E') {
-        return c;
-      }
-    }
-    return '0';
-  }
-
-  //returns true if passed direction and degree is a valid Latitude coordinate and false otherwise
-  public boolean validLatitude(char direction, double degree){
-    if(direction == '0' || direction == 'N' || direction == 'S'){
-      if(degree <= 41 && degree >= 37) return true;
-    }
-    return false;
-  }
-
-  //returns true if passed direction and degree is a valid longitude coordinate and false otherwise
-  public boolean validLongitude(char direction, double degree){
-    if(direction == '0' || direction == 'W' || direction == 'E'){
-      if(degree <= -102.05 && degree >= -109.05) return true;
-    }
-    return false;
-  }
-
-  public double flipDegrees(char direction, double degree){
-    if(direction == 'S' || direction == 'W')return -degree;
-    else return degree;
-  }
-
-  //accepts location string without direction containing 0 spaces
-  //returns array containing degrees minutes and seconds which are allocated to 0 if they weren't in the passed location
-  public double[] getLocationArray(String coordinate){
-    double[] location = {0,0,0};
-    boolean isSimplified = true;
-    try {
-      if (coordinate.contains("°")) {
-        isSimplified = false;
-        int index = coordinate.indexOf('°');
-        location[0] = Double.parseDouble(coordinate.substring(0, index));
-        coordinate = coordinate.substring(index+1);
-      }
-      if (coordinate.contains("'")) {
-        isSimplified = false;
-        int index = coordinate.indexOf('\'');
-        location[1] = Double.parseDouble(coordinate.substring(0, index));
-        coordinate = coordinate.substring(index+1);
-      }
-      if (coordinate.contains("\"")) {
-        isSimplified = false;
-        int index = coordinate.indexOf('"');
-        location[2] = Double.parseDouble(coordinate.substring(0, index));
-        coordinate = coordinate.substring(index+1);
-      }
-      if (isSimplified && coordinate !="") location[0] = Double.parseDouble(coordinate);
-      return location;
-    }catch (NumberFormatException e){
-      return null;
-    }
-  }
-
-
-  //accepts a coordinate string and a boolean that is true for latitude and false for longitude
-  //parses coordinate string returning coordinate string converted to decimal degrees
-  //returns -1000 if string is invalid
-  public double parseLatLong(String coordinate, boolean isLatitude){
-    coordinate = coordinate.trim();  //trim spaces from front and back of string
-    Pattern format = Pattern.compile("^\\s*(?:(?:-?\\d+(?:\\.\\d+)?)|(?:-?\\d+(?:\\.\\d+)?\\s*°)?(?:\\s*-?\\d+(?:\\.\\d+)?\\s*['|`|'|‘|’|′])?(?:\\s*-?\\d+(?:\\.\\d+)?\\s*[\"|”|“|″])?)\\s*[N|S|E|W]?\\s*$");
-    Matcher verifier = format.matcher(coordinate);
-    if(verifier.matches()) {  //check if string matches format
-      char direction = hasDirection(coordinate);  //parse direction from string
-      coordinate.replaceAll("['|`|'|‘|’|′]","'");
-      coordinate.replaceAll("[\"|”|“|″]","\"");
-      coordinate = coordinate.replaceAll("[N|S|E|W]","");  //remove direction from string
-      coordinate = coordinate.replaceAll("\\s+", "");   //remove unnecessary spaces to prepare for split into array
-      double[] location = getLocationArray(coordinate);
-
-      if(location == null) return -1000;
-
-      double decimalDegrees = convertDecimalDegrees(location);
-      decimalDegrees = flipDegrees(direction,decimalDegrees);
-
-      if(isLatitude) {
-        if (validLatitude(direction, decimalDegrees)) return decimalDegrees;
-      }else if(!isLatitude){
-        if(validLongitude(direction,decimalDegrees)) return decimalDegrees;
-      }
-    }
-    return -1000;
-  }
 
   public double convertToRadian(double angle) {
 //    if (angle.isEmpty()) angle += "0";
@@ -124,16 +22,16 @@ public class CoordinateDistance {
     return angle * (Math.PI / 180);
   }
 
-  public int greatCirDist(String latitude1, String longitude1, String latitude2, String longitude2) {
+  public int greatCirDist(double latitude1, double longitude1, double latitude2, double longitude2) {
     double deltaX, deltaY, deltaZ, radius, chordLen, centralAngle;
 
     if (this.distance == "kilometers")
       radius = 6371.0088;
     else radius = 3958.7613;
-    double decLat1 = this.convertToRadian(parseLatLong(latitude1,true));
-    double decLat2 = this.convertToRadian(parseLatLong(latitude2,true));
-    double decLong1 = this.convertToRadian(parseLatLong(longitude1,false));
-    double decLong2 = this.convertToRadian(parseLatLong(longitude2,false));
+    double decLat1 = this.convertToRadian(latitude1);
+    double decLat2 = this.convertToRadian(latitude2);
+    double decLong1 = this.convertToRadian(longitude1);
+    double decLong2 = this.convertToRadian(longitude2);
     if(decLat1 == -1 || decLat2 == -1 || decLong1 == -1 || decLong2 == -1)return -1;
     deltaX = Math.cos(decLat2) * Math.cos(decLong2) -
             Math.cos(decLat1) * Math.cos(decLong1);
--- server/src/test/java/com/tripco/t10/planner/TestDist.java
+++ server/src/test/java/com/tripco/t10/planner/TestDist.java
@@ -46,53 +46,18 @@ public class TestDist {
   }
 
   @Test
-  public void testDistances() {
-    assertEquals(-1, distance.greatCirDist("", "","", ""));  //not in colorado
-    assertEquals(-1, distance.greatCirDist("40.445", "-79.982","20.445", "-79.982"));  //not in colorado
-    assertEquals(-1, distanceKM.greatCirDist("40.445", "-79.982","20.445", "-79.982"));  //not in colorado
+  public void testDistancesFail() {
+    assertEquals(0, distance.greatCirDist(0, 0,0, 0));  //not in colorado
+    assertEquals(1382, distance.greatCirDist(40.445, -79.982,20.445, -79.982));  //not in colorado
+    assertEquals( 2224, distanceKM.greatCirDist(40.445, -79.982,20.445, -79.982));  //not in colorado
   }
 
   @Test
-  public void testHasDirection() {
-    assertEquals('N',distance.hasDirection("88.2°20.34'38.1\"N"));
-    assertEquals('E',distance.hasDirection("88.2°20.34'38.1\"E"));
-    assertEquals('0',distance.hasDirection("88.2°20.34'38.1"));
-  }
-
-  @Test
-  public void testGetLocationArray(){
-    assertArrayEquals(new double[]{88.2,20.34,38.1},distance.getLocationArray("88.2°20.34'38.1\""),.00000001);
-    assertArrayEquals(new double[]{0,0,0},distance.getLocationArray(""),.00000001);
-    assertArrayEquals(new double[]{0,3.2,0},distance.getLocationArray("3.2'"),.00000001);
+  public void testDistancesPass() {
+    assertEquals(24,distance.greatCirDist(39.7392, -104.9903, 40.0149900, -105.2705500));
+    assertEquals(41, distance.greatCirDist(40.0149900, -105.2705500, 40.585258, -105.084419));
+    assertEquals(59, distance.greatCirDist(40.585258, -105.084419, 39.7392, -104.9903));
   }
 
 
-  @Test
-  public void testParseLat() {
-    assertEquals(38.2 + (20.34 / 60) + (38.1 / 3600.0), distance.parseLatLong("38.2°20.34'38.1\"N", true),.00000001);  //test no spaces
-    assertEquals(39.35 + (32.0 / 60) + (42.12 / 3600.0), distance.parseLatLong("    39.35  ° 32   ' 42.12   \" N  ", true),.00000001);  //test latitude general form and lots of spaces
-    assertEquals(40.35 - (32.0 / 60.0) - (42.12 / 3600.0), distance.parseLatLong("-40.35° 32' 42.12\" S", true),.00000001); //test negative sign
-    assertEquals(37.0 + (42.12 / 3600.0), distance.parseLatLong("37° 42.12\" N", true),.00000001);  //test exclude degrees from expression
-    assertEquals(37.21, distance.parseLatLong("-37.21 S", true),.00000001);  //test decimal degrees plus direction
-  }
-
-  @Test
-  public void testParseLong() {
-    assertEquals(-108.2+(20.64/60.0)+(38.1/3600.0), distance.parseLatLong("-108.2° 20.64' 38.1\" E",false),.00000001); //test longitude general form
-    assertEquals(-102.2-(20.0/60.0)-(100.12/3600.0), distance.parseLatLong("102.2° 20' 100.12\" W",false),.00000001);  //test west and carryover from seconds
-    assertEquals(-107.32, distance.parseLatLong("   -107.32   ",false),.00000001);  //test already in format longitude degrees and extra spaces
-    assertEquals(-104.95, distance.parseLatLong("104.95° W",false),.00000001);  //test only degrees
-    assertEquals(-105.95 + (32.1/60.0), distance.parseLatLong("-105.95° 32.1'",false),.00000001);  //test no direction in full format
-  }
-
-  @Test
-  public void testParseLatLongFail() {
-    assertEquals(-1000, distance.parseLatLong("40.95°60' 43\" N",true),.00000001);  //test that latitude cant go over 90
-    assertEquals(-1000, distance.parseLatLong("109.95°60' 43\" E",false),.00000001);  //test that longitude cant go over 180;
-    assertEquals(-1000, distance.parseLatLong("49.95°60' 43\" E",true),.00000001);  //test that latitude cant be E or W
-    assertEquals(-1000, distance.parseLatLong("59.95°60' 43\" S",false),.00000001);  //test that longitude cant be north south
-    assertEquals(-1000, distance.parseLatLong("        ",true),.00000001);
-    assertEquals(-1000,distance.parseLatLong("20' 43 39.95° N", true),.00000001); //test out of order
-    assertEquals(-1000,distance.parseLatLong("N",true),.00000001);
-  }
 }
