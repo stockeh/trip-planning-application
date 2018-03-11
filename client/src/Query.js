7e7bc0a306d7e7849a64e12c12080b06291eded7
Jason Stock <stock@localhost.localdomain>
2018-03-11 10:13:59 -0600

    Destinations in tables as recieved from server response, closes #161

--- client/src/Query.js
+++ client/src/Query.js
@@ -15,11 +15,13 @@ class Query extends Component {
         type    : "query",
         query   : "",
         places  : []
-      },
-      destination: ""
+      }
     };
     this.updateSearch = this.updateSearch.bind(this);
     this.updateQuery = this.updateQuery.bind(this);
+    this.buildDestination = this.buildDestination.bind(this);
+    this.updateDestinations = this.updateDestinations.bind(this);
+    this.createTable = this.createTable.bind(this);
   }
 
   updateSearch(tffi){
@@ -30,11 +32,47 @@ class Query extends Component {
 
   updateQuery(event) {
     let newSearch = Object.assign({}, this.state.search);
-    newSearch.query = event.target.value
-    this.setState({search: newSearch})
+    newSearch.query = event.target.value;
+    this.setState({search: newSearch});
+  }
+
+  updateDestinations(index) {
+    let newSearch = Object.assign({}, this.state.search);
+    let removedItem = newSearch.places.splice(index, 1);
+    // this.props.updatePlaces(removedItem);
+    this.setState({search: newSearch});
+  }
+
+  buildDestination(item, index) {
+    let destinationName = [item.name];
+    destinationName.push(<a key={item.name} className="text-info font-weight-light"
+                            style={{cursor:'pointer'}}
+                            onClick={() => {
+                              this.updateDestinations(index);
+                              }}><br/><small>Add to Trip!</small></a>);
+    return destinationName;
+  }
+
+  createTable() {
+    let destinations = this.state.search.places.map((item, index) => <td>{this.buildDestination(item, index)}</td>);
+    let table = [];
+    for (let i = 0; i < destinations.length; ++i) {
+        table.push(<tr key={i}>{ destinations[i] }</tr>);
+    }
+    return (
+      <table className="table table-responsive table-hover">
+          <thead>
+            <tr><th className="table-info align-middle" scope="col">Destinations</th></tr>
+          </thead>
+          <tbody>
+            {table}
+          </tbody>
+      </table>
+    );
   }
 
   render() {
+
     return(
       <div id="query">
         <button type="button" className="btn btn-primary btn-sm" data-toggle="modal" data-target="#customSearchModal">Look Up</button>
@@ -53,7 +91,7 @@ class Query extends Component {
               <div className="modal-body">
                 <h6>Enter the name of a destination that you would like to visit!</h6>
                 <input id="destination" type="text" className="form-control" onChange={this.updateQuery} placeholder="Destination name..."/>
-                <br/> @todo ... add clickable itinerary to add destinations to tip.
+                <br/> {this.createTable()}
               </div>
 
               <div className="modal-footer">
