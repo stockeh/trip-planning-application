841910bbf0fcfdcbd0f6c2afcea557deb50ef1d1
Evan Steiner <evanjs@localhost.localdomain>
2018-03-14 17:19:29 -0600

    changing things to make code climate happy, also removed Seattle from places list

--- server/src/main/java/com/tripco/t10/planner/Search.java
+++ server/src/main/java/com/tripco/t10/planner/Search.java
@@ -2,7 +2,10 @@ package com.tripco.t10.planner;
 
 import com.mysql.jdbc.StringUtils;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Search {
@@ -12,19 +15,33 @@ public class Search {
   public String query;
   public ArrayList<Place> places;
 
-  // db configuration information
-  private String baseSearch = "select id,name,municipality,latitude,longitude,type from airports where id = ";
-  private final static String myDriver = "com.mysql.jdbc.Driver";
-  private final static String myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
+  private String baseSearch = "select id,name,municipality," +
+    "latitude,longitude,type from airports where id = ";
+  private static final String myDriver = "com.mysql.jdbc.Driver";
+  private static final String myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
 
-  //call with args[0] = username args[1] = password and args[2] = query
+  /**
+   * Some defaults for the Search object
+   */
+  public Search(){
+    this.version = 2;
+    this.type = "query";
+    this.query = "";
+  }
+
+  /**
+   * For testing the search function off campus
+   * @param args  args[0] = query
+   */
   public static void main(String[] args){
-    String search = "select id,name,municipality,latitude,longitude,type from airports where name like '%" + args[2] +"%'" +
-      "or municipality like '%" + args[2] +"%' order by municipality,name limit 10";
+    String search = "select id,name,municipality,latitude," +
+      "longitude,type from airports where name like '%" + args[0] +"%'" +
+      "or municipality like '%" + args[2] +"%' " +
+      "order by municipality,name limit 10";
     try {
       Class.forName(myDriver);
 // connect to the database and query
-      try (Connection conn = DriverManager.getConnection(myUrl, args[0], args[1]);
+      try (Connection conn = DriverManager.getConnection(myUrl, "evanjs", "830960621");
            Statement stCount = conn.createStatement();
            ResultSet rsCount = stCount.executeQuery(search);
       ) {
@@ -39,6 +56,10 @@ public class Search {
     }
   }
 
+  /**
+   * decides which columns to search in based on query and
+   * sets up the search query accordingly
+   */
   public void searchColumns(){
     if(query.matches(".*\\d+.*")){
       baseSearch += "or latitude = '" + query + "'or longitude = '" + query + "'";
@@ -52,27 +73,29 @@ public class Search {
     }
   }
 
+  /**
+   * searches database from constructed query and
+   * updates places variable of object accordingly
+   */
   public void find(){
     baseSearch += "'" + query +"'";
     this.searchColumns();
-    baseSearch += "limit 10";
+    baseSearch += " limit 10";
     try {
       Class.forName(myDriver);
-// connect to the database and query
+      // connect to the database and query
       try (Connection conn = DriverManager.getConnection(myUrl, "evanjs", "830960621");
            Statement sState = conn.createStatement();
            ResultSet rState = sState.executeQuery(baseSearch);
       ) {
         while(rState.next()){
-          places.add(new Place(rState.getString("id"),rState.getString("name"),
+          this.places.add(new Place(rState.getString("id"),rState.getString("name"),
             rState.getString("latitude"),rState.getString("longitude")));
         }
       }
     } catch (Exception e) {
       System.err.println("Exception: "+e.getMessage());
     }
-
-    places.add(new Place("Sea","Seattle","47.6062° N","122.3321° W"));
   }
 
 }
