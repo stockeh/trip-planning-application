104ff635ca919d787c64acaf450c338ea525bf0d
Jason Stock <stock@dover.cs.colostate.edu>
2018-03-17 18:53:29 -0600

    Query code smeels resolved. #218 #144

--- client/src/Query.js
+++ client/src/Query.js
@@ -14,10 +14,13 @@ class Query extends Component {
     };
     this.updateDataPlaces = this.updateDataPlaces.bind(this);
     this.updateQuery = this.updateQuery.bind(this);
-    this.buildDestination = this.buildDestination.bind(this);
     this.updateDestinations = this.updateDestinations.bind(this);
+
+    this.createDestination = this.createDestination.bind(this);
     this.createTable = this.createTable.bind(this);
     this.loadedQuery = this.loadedQuery.bind(this);
+
+    this.modalContent = this.modalContent.bind(this);
   }
 
   updateDataPlaces(tffiPlaces) {
@@ -35,7 +38,7 @@ class Query extends Component {
     this.setState({places: newPlaces});
   }
 
-  buildDestination(item, index) {
+  createDestination(item, index) {
     let destinationName = [item.name];
     destinationName.push(<a key={item.name} className="text-info font-weight-light"
                             style={{cursor:'pointer'}}
@@ -47,7 +50,7 @@ class Query extends Component {
 
   createTable() {
     let dest = this.state.places.map((item, index) => {
-        return <td>{this.buildDestination(item, index)}</td>;
+        return <td>{this.createDestination(item, index)}</td>;
     });
     let table = [];
     for (let i = 0; i < dest.length; ++i) {
@@ -74,6 +77,29 @@ class Query extends Component {
     }
   }
 
+  modalContent() {
+    return (
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="searchModalLabel">Build a Custom Trip!</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <div className="modal-body">
+          <h6>Enter the name of a destination that you would like to visit!</h6>
+          <input id="destination" type="text" className="form-control" onChange={this.updateQuery} placeholder="Destination name..."/>
+          <br/> {this.createTable()}
+        </div>
+
+        <div className="modal-footer">
+          <Search precheck={this.props.precheck} trip={this.props.trip} setPreCheck={this.props.setPreCheck} updateDataPlaces={this.updateDataPlaces}/>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     this.loadedQuery();
     return(
@@ -82,27 +108,10 @@ class Query extends Component {
 
         <div className="modal fade" id="customSearchModal" tabIndex="-1" role="dialog" aria-labelledby="searchModalLabel" aria-hidden="true">
           <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-
-              <div className="modal-header">
-                <h5 className="modal-title" id="searchModalLabel">Build a Custom Trip!</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-
-              <div className="modal-body">
-                <h6>Enter the name of a destination that you would like to visit!</h6>
-                <input id="destination" type="text" className="form-control" onChange={this.updateQuery} placeholder="Destination name..."/>
-                <br/> {this.createTable()}
-              </div>
-
-              <div className="modal-footer">
-                <Search precheck={this.props.precheck} trip={this.props.trip} setPreCheck={this.props.setPreCheck} updateDataPlaces={this.updateDataPlaces}/>
-              </div>
-            </div>
+            {this.modalContent()}
           </div>
         </div>
+
       </div>
     )
   }
