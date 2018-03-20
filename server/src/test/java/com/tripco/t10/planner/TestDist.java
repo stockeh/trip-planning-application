cfdca9a8831c9747b46918f6c81c3cef5cdc6e19
Brian <brianlmart@gmail.com>
2018-03-20 09:37:54 -0600

    Added and updated new test cases to work with userUnit

--- server/src/test/java/com/tripco/t10/planner/TestDist.java
+++ server/src/test/java/com/tripco/t10/planner/TestDist.java
@@ -16,23 +16,30 @@ import static org.junit.Assert.assertTrue;
 public class TestDist {
   Distance distance;
   Distance distanceKM;
+  Distance distanceUser;
+  Option options1 = new Option("miles", "", "", 0);
+  Option options2 = new Option("kilometers", "", "", 0);
+  Option options3 = new Option("user defined", "inches", "123456", 0);  //RADIUS NOT ACTUAL SIZE!!!!!!!!!!!!!!!!!!
 
   // Setup to be done before every test in TestDist
   @Before
   public void initialize() {
-    distance = new Distance("miles");
-    distanceKM = new Distance("kilometers");
+    distance = new Distance(options1);
+    distanceKM = new Distance(options2);
+    distanceUser = new Distance(options3);
   }
 
   @Test
   public void testUnits() {
-    assertEquals("miles", distance.distance);
-    assertEquals("kilometers", distanceKM.distance);
+    assertEquals("miles", options1.distance);
+    assertEquals("kilometers", options2.distance);
+    assertEquals("user defined", options3.distance);
   }
 
   @Test
   public void testDistancesFail() {
     assertEquals(0, distance.greatCirDist(0, 0,0, 0));  //not in colorado
+    assertEquals(0, distanceUser.greatCirDist(0, 0,0, 0));  //not in colorado
     assertEquals(1382, distance.greatCirDist(40.445, -79.982,20.445, -79.982));  //not in colorado
     assertEquals( 2224, distanceKM.greatCirDist(40.445, -79.982,20.445, -79.982));  //not in colorado
   }
@@ -42,6 +49,7 @@ public class TestDist {
     assertEquals(24,distance.greatCirDist(39.7392, -104.9903, 40.0149900, -105.2705500));
     assertEquals(41, distance.greatCirDist(40.0149900, -105.2705500, 40.585258, -105.084419));
     assertEquals(59, distance.greatCirDist(40.585258, -105.084419, 39.7392, -104.9903));
+    assertEquals(1266, distanceUser.greatCirDist(40.0149900, -105.2705500, 40.585258, -105.084419));
   }
 
 
