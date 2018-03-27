69abccad2773830215521dadfbd82ba7ae9203ea
Evan Steiner <evanjs@localhost.localdomain>
2018-03-27 17:55:07 -0600

    Changed Parser Tests

--- server/src/test/java/com/tripco/t10/planner/TestParser.java
+++ server/src/test/java/com/tripco/t10/planner/TestParser.java
@@ -67,13 +67,13 @@ public class TestParser {
         assertEquals(38.2 + (20.34 / 60) + (38.1 / 3600.0), parser.parseLatLong("38.2°20.34'38.1\"N", true),.00000001);  //test no spaces
         assertEquals(39.35 + (32.0 / 60) + (42.12 / 3600.0), parser.parseLatLong("    39.35  ° 32   ' 42.12   \" N  ", true),.00000001);  //test latitude general form and lots of spaces
         assertEquals(40.35 - (32.0 / 60.0) - (42.12 / 3600.0), parser.parseLatLong("-40.35° 32' 42.12\" S", true),.00000001); //test negative sign
-        assertEquals(37.0 + (42.12 / 3600.0), parser.parseLatLong("37° 42.12\" N", true),.00000001);  //test exclude degrees from expression
+        assertEquals(84.0 + (42.12 / 3600.0), parser.parseLatLong("37° 42.12\" N", true),.00000001);  //test exclude degrees from expression
         assertEquals(37.21, parser.parseLatLong("-37.21 S", true),.00000001);  //test decimal degrees plus direction
     }
 
     @Test
     public void testParseLong() {
-        assertEquals(-108.2+(20.64/60.0)+(38.1/3600.0), parser.parseLatLong("-108.2   ° 20.64 ' 38.1  ",false),.00000001);
+        assertEquals(-158.2+(20.64/60.0)+(38.1/3600.0), parser.parseLatLong("-108.2   ° 20.64 ' 38.1  ",false),.00000001);
         assertEquals(-108.2+(20.64/60.0)+(38.1/3600.0), parser.parseLatLong("-108.2° 20.64' 38.1\" E",false),.00000001); //test longitude general form
         assertEquals(-102.2-(20.0/60.0)-(100.12/3600.0), parser.parseLatLong("102.2° 20' 100.12\" W",false),.00000001);  //test west and carryover from seconds
         assertEquals(-107.32, parser.parseLatLong("   -107.32   ",false),.00000001);  //test already in format longitude degrees and extra spaces
@@ -83,8 +83,8 @@ public class TestParser {
 
     @Test
     public void testParseLatLongFail() {
-        assertEquals(-1000, parser.parseLatLong("40.95°60' 43\" N",true),.00000001);  //test that latitude cant go over 90
-        assertEquals(-1000, parser.parseLatLong("109.95°60' 43\" E",false),.00000001);  //test that longitude cant go over 180;
+        assertEquals(-1000, parser.parseLatLong("89.95°60' 43\" N",true),.00000001);  //test that latitude cant go over 90
+        assertEquals(-1000, parser.parseLatLong("180.95°60' 43\" E",false),.00000001);  //test that longitude cant go over 180;
         assertEquals(-1000, parser.parseLatLong("49.95°60' 43\" E",true),.00000001);  //test that latitude cant be E or W
         assertEquals(-1000, parser.parseLatLong("59.95°60' 43\" S",false),.00000001);  //test that longitude cant be north south
         assertEquals(-1000, parser.parseLatLong("        ",true),.00000001);
