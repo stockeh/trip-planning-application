c779d75500c4f630cb93db7309f6eece5264ceab
Evan Steiner <evanjs@localhost.localdomain>
2018-03-31 14:53:34 -0600

    better searching, id field now searched with LIKE instead of =

--- server/src/main/java/com/tripco/t10/planner/Search.java
+++ server/src/main/java/com/tripco/t10/planner/Search.java
@@ -15,8 +15,8 @@ public class Search {
   public String query;
   public ArrayList<Place> places;
 
-  private String baseSearch = "select id,name,municipality,"
-    + "latitude,longitude,type from airports where id = ";
+  private String baseSearch = "SELECT id,name,municipality,"
+    + "latitude,longitude,type FROM airports WHERE ";
   private static final String myDriver = "com.mysql.jdbc.Driver";
   private static final String myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
 
@@ -30,57 +30,18 @@ public class Search {
   }
 
   /**
-   * For testing the search function off campus.
-   * @param args  args[0] = query
-   */
-  public static void main(String[] args){
-    String search = "select id,name,municipality,latitude,"
-      + "longitude,type from airports where name like '%" + args[0] +"%'"
-      + "or municipality like '%" + args[2] +"%' "
-      + "order by municipality,name limit 10";
-    try {
-      Class.forName(myDriver);
-// connect to the database and query
-      try (Connection conn = DriverManager.getConnection(myUrl, "evanjs", "830960621");
-           Statement stCount = conn.createStatement();
-           ResultSet rsCount = stCount.executeQuery(search);
-      ) {
-        while (rsCount.next()) {
-          System.out.printf(" \"%s\"", rsCount.getString("name") + " " + rsCount.getString("id"));
-          System.out.println();
-        }
-//        System.out.printf(" ]\n}\n");
-      }
-    } catch (Exception e) {
-      System.err.println("Exception: "+e.getMessage());
-    }
-  }
-
-  /**
-   * decides which columns to search in based on query and
-   * sets up the search query accordingly.
-   */
-  public void searchColumns(){
-    if(query.matches(".*\\d+.*")){
-      baseSearch += "or latitude = '" + query + "'or longitude = '" + query + "'";
-      baseSearch += " order by id,latitude,longitude";
-    }else if(query.contains("airport") || query.contains("heliport")){
-      baseSearch +="or name like '%" + query + "%' or type like '%" + query + "%'";
-      baseSearch += " order by id,name,type";
-    }else{
-      baseSearch +="or municipality like '%" + query + "%' or name like '%" + query + "%'";
-      baseSearch += " order by id,municipality,name";
-    }
-  }
-
-  /**
    * searches database from constructed query and
    * updates places variable of object accordingly.
    */
   public void find(){
-    baseSearch += "'" + query +"'";
-    this.searchColumns();
-    baseSearch += " limit 10";
+    baseSearch += "name LIKE '%" + query
+      + "%' OR id LIKE '%" + query
+      + "%' OR municipality LIKE '%" + query
+      + "%' OR type LIKE '%" + query
+      + "%' OR longitude LIKE '%" + query
+      + "%' OR latitude LIKE '%" + query
+      + "%' limit 15";
+    System.out.println(baseSearch);
     try {
       Class.forName(myDriver);
       // connect to the database and query
