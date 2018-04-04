6cdf866afa4017cd8abfcb0ae95a06014c6bafad
Evan Steiner <evanjs@localhost.localdomain>
2018-04-03 18:00:18 -0600

    New config class is now working, made more changes to Microserver

--- server/src/main/java/com/tripco/t10/planner/Config.java
+++ server/src/main/java/com/tripco/t10/planner/Config.java
@@ -1,4 +1,28 @@
 package com.tripco.t10.planner;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import spark.Request;
+
 public class Config {
+  public String type = "config";
+  public String version = "3";
+  public String optimization = "1";
+  public String distances[] = {"miles","kilometers","nautical miles","user defined"};
+  public String maps[] = {"svg"};
+  public String filters[];
+
+
+  public Config (Request request){
+    // plan the trip.
+    this.findFilters();
+  }
+
+  private void findFilters(){
+
+  }
+
+
+
 }
