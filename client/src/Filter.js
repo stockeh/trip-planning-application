8667996cc3b96f68bc76e3ab23424be71911376e
Jason Stock <stock@localhost.localdomain>
2018-04-05 01:04:29 -0600

    Checkbox added - #281

--- client/src/Filter.js
+++ client/src/Filter.js
@@ -1,25 +1,39 @@
 import React, {Component} from 'react';
 import FaFilter from 'react-icons/lib/fa/filter';
 
-/* Adds the component to build a custom trip
- * Renders a modal to get input and print
- * out an itinerary of destinations to choose
- * from.
+/** Adds the component to select filtered items for
+ * database search.
  */
 class Filter extends Component {
   constructor(props){
     super(props);
-    this.state = { };
+    this.state = {
+      filters : [{ attribute : "type",
+                     values  : [ "balloonport","heliport", "airport"] }] };
+    this.renderCheckbox = this.renderCheckbox.bind(this);
   }
 
+  renderCheckbox(value) {
+    return <div className="checkbox">
+      <label><input type="checkbox" id={value} value={value} onChange={console.log("Fix")}/> {value} </label>
+    </div>;
+  }
 
   render() {
-
+    let attributes = [];
+    for (let index in this.state.filters) {
+      attributes.push(<h6 className="text-uppercase">{this.state.filters[index].attribute}</h6>);
+      for (let value of this.state.filters[index].values) {
+        attributes.push(this.renderCheckbox(value));
+        console.log("Value: " + value);
+      }
+    }
     return(
       <div id="filter">
         <button type="button" className="btn btn-light btn-md">
           <FaFilter/>
         </button>
+        {attributes}
       </div>
     )
   }
