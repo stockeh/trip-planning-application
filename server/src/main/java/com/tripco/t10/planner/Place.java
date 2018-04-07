9561fe8de934897e00f3794b1e5a47773398ba76
Evan Steiner <evanjs@indianapolis.cs.colostate.edu>
2018-04-07 13:20:55 -0600

    changed some methods to private, added more fields to place object

--- server/src/main/java/com/tripco/t10/planner/Place.java
+++ server/src/main/java/com/tripco/t10/planner/Place.java
@@ -9,12 +9,38 @@ public class Place {
   public String name;
   public String latitude;
   public String longitude;
+  public String country;
+  public String region;
+  public String municipality;
+  public String continent;
 
+  /**
+   * Place constructor is used for testing.
+   * @param id is id of place
+   * @param name is name of place
+   * @param latitude is latitude of place
+   * @param longitude is longitude of place
+   */
   public Place(String id, String name, String latitude, String longitude){
     this.id = id;
     this.name = name;
     this.latitude = latitude;
     this.longitude = longitude;
+  }
+
+  /**
+   * Place constructor used by Search to give place more information
+   * @param place is an array of all place variables
+   */
+  public Place(String[] place){
+    this.continent = place[0];
+    this.region = place[1];
+    this.country = place[2];
+    this.municipality = place[3];
+    this.latitude = place[4];
+    this.longitude = place[5];
+    this.name = place[6];
+    this.id = place[7];
   }
 }
 
--- server/src/main/java/com/tripco/t10/planner/Search.java
+++ server/src/main/java/com/tripco/t10/planner/Search.java
@@ -39,7 +39,7 @@ public class Search {
    * Adds AND statements to query based on the filter array it receives.
    * @param filter is the filter to iterate over.
    */
-  public void iterateFilter(Filter filter){
+  private void iterateFilter(Filter filter){
     join += "AND ("; //open combination statement
     for(int index = 0; index < filter.size(); index++){
       if(index > 1){
@@ -55,7 +55,7 @@ public class Search {
    * Calls iterateFilter method for actual construction of statement.
    *
    */
-  public void constructQueryFromFilters(){
+  private void constructQueryFromFilters(){
     if(filters != null && filters.length > 0){
       for(Filter f : filters){
         if(f.getAttribute()!="" && !f.isEmpty()) {
@@ -66,10 +66,9 @@ public class Search {
   }
 
   /**
-   * Constructs query and calls accessDatabase to perform query.
-   * Constructs places array from query results
+   * Constructs query for database search.
    */
-  public void find() {
+  private void createQueryStatement(){
     join += "(a.name LIKE '%" + query + "%' OR country.name LIKE '%" + query
             + "%' OR region.name LIKE '%" + query + "%' OR continents.name LIKE '%" + query
             + "%' OR a.id LIKE '%" + query
@@ -80,6 +79,14 @@ public class Search {
     constructQueryFromFilters();
     join += orderBy;
     System.out.println(join);
+  }
+
+  /**
+   * Performs query on database.
+   * Constructs places array from query results.
+   */
+  public void find() {
+    createQueryStatement();
     try {
       Class.forName(myDriver);
       // connect to the database and query
@@ -88,9 +95,11 @@ public class Search {
            ResultSet rState = sState.executeQuery(join);
       ) {
         while (rState.next()) {
-          this.places.add(new Place(rState.getString("id"), rState.getString("name"),
-                  rState.getString("latitude"), rState.getString("longitude")));
-
+          String[] place = {rState.getString(("name")),rState.getString("name"),
+                  rState.getString("name"),rState.getString("municipality"),
+                  rState.getString("latitude"), rState.getString("longitude"),
+                  rState.getString("name"), rState.getString("id")};
+          this.places.add(new Place(place));
         }
       }
     } catch (Exception e) {
