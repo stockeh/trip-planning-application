d08eb62b991d98a3d6b619bc2dbdabfe61087422
Jason Stock <stock@honolulu.cs.colostate.edu>
2018-04-14 20:25:41 -0600

    Header and footer are responseive - closes #381

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './css/header.css'
 
 /* Renders a text footer below the application with copyright
  * and other useful information.
@@ -10,15 +11,19 @@ class Footer extends Component {
 
   render() {
     return (
-      <div className="add-header-height">
-
-        <h4 style={{color: 'white'}}>© TripCo t{this.props.number} {this.props.name} 2018</h4>
-
-        <a href="http://www.colostate.edu/">
-          <img src="../resources/signature-oneline.svg"
-               alt="Colorado State University" style={{width: 350, height: 58}}/>
-        </a>
-      </div>
+      <div className="add-footer-height row">
+        <div className="col-lg-5 col-md-5 col-sm-12 footer-copyright">
+          <h4 className="larger-CSUtext-uncap" style={{color: 'white'}}>
+            © TripCo - t{this.props.number} {this.props.name} 2018
+          </h4>
+        </div>
+        <div className="col">
+          <a href="http://www.colostate.edu/">
+            <img className="img-resize" src="../resources/CSU-Official-wrdmrk-Rev.png"
+                 alt="Colorado State University"/>
+          </a>
+        </div>
+    </div>
     )
   }
 }
